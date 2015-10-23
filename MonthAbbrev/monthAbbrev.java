40eae97e15c080fcceb83aee63490ca0ddf94c9d
Alec Burgdorf <aburgd@users.noreply.github.com>
2015-10-23 17:59:01 -0500

    finished initial abbrev project

--- MonthAbbrev/monthAbbrev.java
+++ MonthAbbrev/monthAbbrev.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 
-public class MonthAbbrev
-{
-	public static void main (String[] args)
-	{
-		Scanner keyboard = new Scanner(System.in);
+public class MonthAbbrev {
+	public static void main (String[] args)	{
 		
+		// Scanner dec
+		Scanner keyboard = new Scanner(System.in);
+        
+        // Ask user
+        System.out.print("To see the first three letters of a month,");
+        System.out.println(" enter a month number (1-12): ");
+        int number = keyboard.nextInt();
+        
+        // Months as single string
+        final String MONTH_TABLE = "JanFebMarAprMayJunJulAugSepOctNovDec";
+        
+        // Compute start, stop, and abbrev
+        int start = (number - 1) * 3;
+        int stop = start + 3;
+        String abbrev = MONTH_TABLE.substring(start,stop);
+        
+        // Print final statement
+        System.out.println("\nMonth #" + number + " begins with '" + abbrev + "'.");
 		
 	}
 }
--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Some might be school projects and classwork, others might be side projects in Ja
 Python sister repo might appear someday if I feel up to it.
 Possibly also Ruby, if I want to experiment.
 
-### Note: No repos have .class files for simplicity's sake.
+#### Note: No repos have .class files for simplicity's sake.
 The Java programs are not compiled in case anyone wants to make edits and suggestions to the program before they compile for their own use.
