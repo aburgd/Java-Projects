2d85bc3bb7d7d6b11da57759aa79d224d9e7de57
Alec Burgdorf <aburgd@users.noreply.github.com>
2015-11-16 09:05:30 -0600

    updated BalloonPrank.java

--- BalloonPrank/BalloonPrank.java
+++ BalloonPrank/BalloonPrank.java
@@ -1,19 +1,31 @@
 import java.util.Scanner;
 
-public class BalloonPrank {
+public class balloonPrank {
+	private static Scanner kbd;
+
 	public static void main(String[] args) {
 
-		final double LENGTH = 9.0;
-		final double WIDTH = 8.0;
-		final double HEIGHT = 7.0;
+		double length;
+		double width;
+		double height;
+		double bRad;
 
 		System.out.print("Enter a balloon radius in inches: ");
-		Scanner kbd = new Scanner(System.in);
-		double bRad = kbd.nextDouble();
+		kbd = new Scanner(System.in);
+		bRad = kbd.nextDouble();
 		 bRad /= 12; // to feet
 		
+		System.out.print("Enter a length in feet: ");
+		length = kbd.nextDouble();
+		
+		System.out.print ("Enter a width in feet: ");
+		width = kbd.nextDouble();
+		
+		System.out.print("Enter a height in feet: ");
+		height = kbd.nextDouble();
+		
 		double bVol = Sphere.volume(bRad);
-		double rVol = Box.volume(LENGTH, WIDTH, HEIGHT);
+		double rVol = Box.volume(length, width, height);
 
 		double bReq = rVol / bVol;
 		System.out.printf("Using balloons with radii of %.0f inches, you will need %.0f balloons.\n", bRad*12, bReq);
