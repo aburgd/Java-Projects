e94e948345159985c2f8b4f26ffd42e3c3be7c88
Alec <aburgd@users.noreply.github.com>
2016-01-09 18:14:40 +0000

    add TShirt.java

---
+++ TShirt/TShirt.java
@@ -0,0 +1,32 @@
+public class TShirt {
+	public TShirt(String size) {
+		switch (size.charAt(0)) {
+			case 'S': case 'M': case 'L':
+				myPrice = 10.00;
+				break;
+			case 'X':
+				switch (size.charAt(1)) {
+					case 'S':
+						myPrice = 9.00;
+						break;
+					case 'L':
+						myPrice = 11.00;
+						break;
+					default:
+						System.err.println("TShirt(size): size \"" + size + "\" is invalid");
+						System.exit(1);
+				}
+			default:
+				System.err.println("TShirt(size): size \"" + size + "\" is invalid");
+				System.exit(1);
+			}
+	}
+
+	public String getSize() { return mySize; }
+	public double getPrice() { return myPrice; }
+
+	public String toString() { return mySize + " ($" + myPrice + ")"; }
+
+	private String mySize;
+	private double myPrice;
+}
