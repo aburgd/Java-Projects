274dc0b655646de8a99b4dd3d7617524842e2a84
Alec Burgdorf <aburgd@users.noreply.github.com>
2016-01-14 08:48:46 -0600

    added toString() to PhLabeler

--- pHLabeler/PhLabeler.java
+++ pHLabeler/PhLabeler.java
@@ -1,19 +1,25 @@
+package pH;
 import java.util.Scanner;
 
-public class PhLabeler {
-	public static void main (String[] args) {
-		Scanner kbd = new Scanner(System.in);
+public class PHLabeler {
 
+	private static Scanner kbd;
+
+	public static void main(String[] args) {
+	
 		boolean done = false;
 		while (!done) {
 			System.out.print("Enter a pH value, or -1 to quit: ");
+			kbd = new Scanner(System.in);
 			double phValue = kbd.nextDouble();
-			if (phValue < PH.MIN) {
+			if (phValue < PH.STRONG_AC_MIN) {
 				done = true;
 			} else {
 				PH pH = new PH(phValue);
-				System.out.println(" --> " + pH.label());
+				System.out.println(" --> " + pH.toString() );
 			}
 		}
+	
 	}
+
 }
