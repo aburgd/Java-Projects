cc1a13e6390b1eaac51dc413f67a2599de7c3f4d
Alec Burgdorf <aburgd@users.noreply.github.com>
2016-04-14 08:16:06 -0500

    added Point.java

---
+++ Shape/Point.java
@@ -0,0 +1,10 @@
+public class Point {
+  public Point(double x, double y) {
+    myX = x; myY = y;
+  }
+  
+  public double getX() { return myX; }
+  public double getY() { return myY; }
+  
+  private double myX, myY;
+}
