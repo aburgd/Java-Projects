0d370ecaf92e9834e81fba5f234e997e19948804
Alec Burgdorf <aburgd@users.noreply.github.com>
2016-04-14 08:26:01 -0500

    changed some - to +
    should plot now

--- Shape/Square.java
+++ Shape/Square.java
@@ -5,8 +5,8 @@ public class Square extends Polygon {
 		super(aColor, 4);
 		double halfSideLength = sideLength / 2;
 		super.setPoint(0, centerX - halfSideLength, centerY - halfSideLength);
-		super.setPoint(1, centerX - halfSideLength, centerY - halfSideLength);
-		super.setPoint(2, centerX - halfSideLength, centerY - halfSideLength);
+		super.setPoint(1, centerX - halfSideLength, centerY + halfSideLength);
+		super.setPoint(2, centerX + halfSideLength, centerY + halfSideLength);
 		super.setPoint(3, centerX - halfSideLength, centerY - halfSideLength);
 	}
 }
