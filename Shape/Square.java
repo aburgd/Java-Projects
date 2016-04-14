747929987c736ee0a6e0b5cd84fd82e802fd658a
Alec Burgdorf <aburgd@users.noreply.github.com>
2016-04-14 08:17:55 -0500

    added Square.java
    subclass of Polygon

---
+++ Shape/Square.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class Square extends Polygon {
+	public Square(Color aColor, double centerX, double centerY, double sideLength) {
+		super(aColor, 4);
+		double halfSideLength = sideLength / 2;
+		super.setPoint(0, centerX - halfSideLength, centerY - halfSideLength);
+		super.setPoint(1, centerX - halfSideLength, centerY - halfSideLength);
+		super.setPoint(2, centerX - halfSideLength, centerY - halfSideLength);
+		super.setPoint(3, centerX - halfSideLength, centerY - halfSideLength);
+	}
+}
