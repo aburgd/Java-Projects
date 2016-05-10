b3d87e34568ac591d233420d5a690eb13f0f5071
aeburgd <aeburgd@gmail.com>
2016-05-10 19:49:43 -0400

    tweaked FinalProject/DHKE.java, needs prime check

--- FinalProject/DHKE.java
+++ FinalProject/DHKE.java
@@ -2,58 +2,61 @@ import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class DHKE {
-	// Keyboard and random number generator
+	
+	// Keyboard set up
 	public static Scanner kbd;
-	public static SecureRandom srng;
+	
+	// Setting up the RNG
+	final static byte[] dummy = new byte[512];
+	public static SecureRandom srng = new SecureRandom();
 	
 	public static void main(String[] args) {
 		kbd = new Scanner(System.in);
+		// Seeding the RNG
+		srng.nextBytes(dummy);
 		
+		// Ask user for input: public key, or shared secret
 		System.out.println("Do you want to compute a public key," +
 		" or a shared secret? ");
 		String answer = kbd.nextLine();
 
 		if ( answer.toLowerCase().contains("public") ) {		 			// Checks if the user wants the public key...
-			int g = 0;
-			System.out.print("Your base number g is: \n" + g);
-			g = srng.nextInt();
+			int baseG = Math.abs( srng.nextInt() );
+			System.out.print("Your base number g is: \n" + baseG + "\n");
 			
-			int p = 0;
-			System.out.print("Your modulo prime p is: \n" + p);
-			p = srng.nextInt();
+			int modP = Math.abs( srng.nextInt() );
+			System.out.print("Your modulo prime p is: \n" + modP + "\n");
 			
-			System.out.print("Please enter your secret prime a: \n");
-			int a = kbd.nextInt();
+			System.out.print("Please enter your secret integer a: \n");
+			int secretA = kbd.nextInt();
 			
-			int key = DHKE.publicKey(g, p, a);
-			System.out.print("Send this to your contact: " + key + "\n");
+			int publicA = DHKE.publicKey(baseG, modP, secretA);
+			System.out.print("Send this to your contact: " + publicA + "\n");
 			
 		} else if ( answer.toLowerCase().contains("shared") ) { 			// or the shared secret.
 			System.out.print("Please enter your shared modulo prime p: \n");
-			int p = kbd.nextInt();
+			int modP = kbd.nextInt();
 			
 			System.out.print("Please enter the public key your contact sent: \n");
-			int b = kbd.nextInt();
+			int baseG = kbd.nextInt();
 			
-			System.out.print("Please enter your secret prime a: \n");
-			int a = kbd.nextInt();
+			System.out.print("Please enter your secret integer a: \n");
+			int secretA = kbd.nextInt();
 			
-			int secret = DHKE.sharedSecret(p, b, a);
-			System.out.print("Compare this to your contact's: " + secret + "\n");
+			int sharedSecret = DHKE.sharedSecret(modP, baseG, secretA);
+			System.out.print("Compare this to your contact's: " + sharedSecret + "\n");
 		} else {
 			System.out.print("Have a nice day!");
 		}
 	}
 	
 	// Finds the public key in accordance with the Diffie-Hellman Key Exchange algorithm
-	public static int publicKey(int g, int p, int a) {
-		return (int) (Math.pow(g, a) % p);
+	public static int publicKey(int baseG, int modP, int secretA) {
+		return (int) (Math.pow(baseG, secretA) % modP);
 	}
 	
 	// Computes the shared secret in accordance with the Diffie-Hellman Key Exchange algorithm
-	public static int sharedSecret(int p, int b, int a) {
-		
-		
-		return (int) (Math.pow(b, a ) % p);
+	public static int sharedSecret(int modP, int publicB, int secretA) {
+		return (int) (Math.pow(publicB, secretA) % modP);
 	}
 }
